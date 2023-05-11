52e9b48fc2ff06aa8d1d8894b22ab11ebbd34e6f
orestun <orest.uzhytchak0001@gmail.com>
2023-05-11 19:11:22 +0300

    added new rmi interface

---
+++ src/main/java/org/rmi/Decomposition.java
@@ -0,0 +1,5 @@
+package org.rmi;
+
+public interface Decomposition {
+    float[][] getSecondPartOfExpression(float[][] Y3, float[][] Y1, float[][] transposedY1);
+}
--- src/main/java/org/rmi/Methods.java
+++
@@ -1,8 +0,0 @@
-package org.rmi;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Methods extends Remote {
-    int sum(int a, int b) throws RemoteException;
-}
