b249a9dd551eefd300cfdce619c00803701fb7eb
orestun <orest.uzhytchak0001@gmail.com>
2023-05-12 22:34:07 +0300

    the last release i hope(

--- src/main/java/org/rmi/Decomposition.java
+++ src/main/java/org/rmi/Decomposition.java
@@ -1,7 +1,8 @@
 package org.rmi;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface Decomposition {
+public interface Decomposition extends Remote {
     float[][] getSecondPartOfExpression(float[][] Y3, float[][] Y1, float[][] transposedY1) throws RemoteException;
 }
